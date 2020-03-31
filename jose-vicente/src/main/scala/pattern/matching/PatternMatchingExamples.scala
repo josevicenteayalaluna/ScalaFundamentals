56bd4ca4d8f3d9c7fa97d4cf1ebcaf2421e594e2
Jose Vicente Ayala Luna <jayala@starbucks.com>
2020-03-31 16:04:09 -0500

    Add pattern matching example

--- jose-vicente/src/main/scala/pattern/matching/PatternMatchingExamples.scala
+++ jose-vicente/src/main/scala/pattern/matching/PatternMatchingExamples.scala
@@ -1,3 +1,30 @@
 package pattern.matching
 
-case class PatternMatchingExamples()
+object PatternMatchingExamples extends App{
+
+  System.out.println("Pattern Matching")
+
+  trait Expression
+  case class Number(number:Int) extends Expression
+  case class Sum(first:Expression,second:Expression) extends Expression
+  case class Prod(first:Expression,second:Expression) extends Expression
+
+  def show(expression1:Expression): String = expression1 match {
+    case Number(number) => s"$number"
+    case Sum(first,second) => show(first)+" + "+ show(second)
+    case Prod(first,second) => {
+      def maybeShowParentesis(expression:Expression): String = expression match {
+        case Sum(_,_) => "(" + show(expression) + ")"
+        case _ => show(expression: Expression)
+      }
+      maybeShowParentesis(first)+" * "+ maybeShowParentesis(second)
+    }
+  }
+
+  private val sumTwoNumbers: Sum = Sum(Number(2), Number(3))
+  private val sumThreeNumbers: Sum = Sum(Sum(Number(89),Number(56)),Number(60))
+  private val prodTwoNumbers: Prod = Prod(Number(78),Sum(Number(6),Number(74)))
+  System.out.println(show(sumTwoNumbers))
+  System.out.println(show(sumThreeNumbers))
+  System.out.println(show(prodTwoNumbers))
+}
